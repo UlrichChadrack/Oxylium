fae7369b92402f0a76908b6e3393de3993ec30f1
UlrichChadrack <124772913+UlrichChadrack@users.noreply.github.com>
2023-05-26 12:15:56 +0100

    Add files via upload

---
+++ resources/views/chirps/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <form method="POST" action="{{ route('chirps.store') }}">
+            @csrf
+            <textarea
+                name="message"
+                placeholder="{{ __('What\'s on your mind?') }}"
+                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+            >{{ old('message') }}</textarea>
+            <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+        </form>
+    </div>
+</x-app-layout>
+
+
+    
+</body>
+</html>
